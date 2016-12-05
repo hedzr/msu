af3db4c7290ef8788355cc8656ea5b6ae5fd96ea
GochoMugo <mugo@forfuture.co.ke>
2016-12-05 08:49:21 +0300

    save 1

--- lib/load.sh
+++ lib/load.sh
@@ -8,5 +8,22 @@
 # shellcheck disable=SC1091
 
 # loading aliases (into env)
-. msu require aliases
+msu_require "aliases"
 
+# we need to perform clean-up, since we are 'source'ng
+unset MSU_EXE
+unset MSU_LIB
+unset MSU_REQUIRE_LOCK
+unset msu__check_deps
+unset msu_require
+unset msu_run
+unset msu_execute
+unset MSU_AUTHOR_NAME
+unset MSU_AUTHOR_EMAIL
+unset MSU_VERSION
+unset MSU_BUILD_HASH
+unset MSU_BUILD_DATE
+unset MSU_INSTALL_LIB
+unset MSU_INSTALL_BIN
+unset MSU_INSTALL_MAN
+unset MSU_INSTALL_LOAD_STRING
